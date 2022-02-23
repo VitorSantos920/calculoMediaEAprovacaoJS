0d36d5056f2a0eb9121d2d3fefc92a07afa4edab
Vitor Santos <pireshugo737@gmail.com>
2022-02-23 17:09:18 -0300

    Commit do programa

---
+++ assets/js/script.js
@@ -0,0 +1,33 @@
+var nomeUser = prompt("Olá, digite seu nome: "); // Requisitando o nome do usuário.
+
+// Boas vindas e instruções
+window.alert("Seja bem-vindo (a): " + nomeUser + ", ao sistema de verificação de aprovação por média!" );
+window.alert("Por gentileza, digite as 4 notas do aluno, para obter sua média.")
+
+// Requisitando as notas 1,2,3,4 e armazenando nas respectivas variáveis (Entrada de Dados)
+var nota1 = parseInt(window.prompt("Digite a 1ª nota: "));
+window.alert("A nota: " + nota1 + " foi inserida com sucesso!");
+
+var nota2 = parseInt(window.prompt("Digite a 2ª nota: "));
+window.alert("A nota: " + nota2 + " foi inserida com sucesso!");
+
+var nota3 = parseInt(window.prompt("Digite a 3ª nota: "));
+window.alert("A nota: " + nota3 + " foi inserida com sucesso!");
+
+var nota4 = parseInt(window.prompt("Digite a 4ª nota: "));
+window.alert("A nota: " + nota4 + " foi inserida com sucesso!");
+
+// Realizando o cáculo da média aritmética (Processamento)
+var media =  (nota1 + nota2 + nota3 + nota4) / 4;
+
+// Exibindo os resultados (média e status de aprovação) ao usuário (Saída de Dados)
+document.getElementById("media").innerHTML = "A média do aluno foi: " + media;
+
+if(media <= 6){ 
+    document.getElementById("status").innerHTML = "REPROVADO!";
+    document.getElementById("status").style.color = "red";
+}else {
+    document.getElementById("status").innerHTML = "APROVADO!";
+    document.getElementById("status").style.color = "green";
+}
+//document.getElementById("media").innerHTML = media;
---
+++ readme.md
@@ -0,0 +1,6 @@
+>Estudo de JavaScript: Variáveis, Entrada, Processamento e Saída de Dados. 
+
+=> Neste software, o usuário poderá calcular médias aritméticas ao inserir 4 notas no sistema.
+=> Após o processamento de soma das notas e cálculo da média, o sistema informará a média.
+=> Se ela for menor ou igual a 6, resultará que o usuário foi reprovado.
+=> Do contrário, resultará que o usuário foi aprovado.
